8f02ce69e894c7ef24db546a75bdbb9df9256e53
smashawk <daichismash@gmail.com>
2020-10-17 23:39:38 +0900

    configファイル修正

--- config/site.js
+++ config/site.js
@@ -1,17 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
+  title: 'Obake Engineer Blog', // Navigation and Site Title
+  titleAlt: 'Obake Engineer Blog', // Title for JSONLD
+  description: '幽霊社員ブログです',
   url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
-  siteLanguage: 'en', // Language Tag on <html> element
+  siteLanguage: 'ja', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
   shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  author: 'smashawk', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
   twitter: '@justinformentin', // Twitter Username
