9823980c7c32f1af10b5938ba33a93bd4d08aee4
obake-fe <daichismash@gmail.com>
2022-08-12 16:30:34 +0900

    feat: seo情報変更

--- config/site.ts
+++ config/site.ts
@@ -2,17 +2,17 @@ export const site = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   title: 'Obake Engineer Blog', // Navigation and Site Title
   titleAlt: 'Obake Engineer Blog', // Title for JSONLD
-  description: '幽霊社員の備忘録です',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  description: '幽霊エンジニアの備忘録です',
+  url: 'https://smashawk.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://smashawk.com/', // url + pathPrefix
   siteLanguage: 'ja', // Language Tag on <html> element
-  logo: 'static/logo/logo.png', // Used for SEO
-  banner: 'static/logo/banner.png',
+  logo: 'static/obake.png', // Used for SEO
+  banner: 'static/obake.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'smashawk', // Author for schemaORGJSONLD
+  shortName: 'Obake Engineer Blog', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'obake_fe', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin' // Twitter Username
+  twitter: '@obake_fe' // Twitter Username
 };
